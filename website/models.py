65ce00f167f2e80ba6505aaa7e99c20cfa194e6b
nonnisnon <nonnisnon@gmail.com>
2021-06-11 13:43:41 +0300

    register bot models add

--- website/models.py
+++ website/models.py
@@ -5,7 +5,14 @@ from sqlalchemy.sql import func
 
 class RegisterBot(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    data = db.Column(db.String(10000))
+    FB_access_token = db.Column(db.String(1000))
+    FB_id_groop = db.Column(db.String(1000))
+    FB_app_secret = db.Column(db.String(1000))
+    FB_app_id = db.Column(db.String(1000))
+    creator_post_skip = db.Column(db.String(1000))
+    chanelSlack = db.Column(db.String(1000))
+    api_bot_secret_Slack = db.Column(db.String(1000))
+
     date = db.Column(db.DateTime(timezone=True), default=func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
@@ -15,4 +22,4 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
-    notes = db.relationship('Note')
+    notes = db.relationship('RegisterBot')
